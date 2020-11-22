efc2449c42985c537a18228d0458e2f04c26c9c6
Josh Ajiniran <josuajiniran@gmail.com>
2020-11-22 20:37:17 +0100

    Refactored using ejs templates

--- app.js
+++ app.js
@@ -5,33 +5,67 @@ const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const ejs = require('ejs')
 
 // models
 const Participant = require('./models/Participant')
 const ClusterReport = require('./models/ClusterReport')
 const StateCoordReport = require('./models/StateCoordReport')
 
-var app = express();
-var multer = require('multer');
+const app = new express();
+
+app.set('view engine', 'ejs')
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 
+var multer = require('multer');
+var storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, '/tmp/my-uploads')
+    },
+    filename: function (req, file, cb) {
+        cb(null, file.fieldname + '-' + Date.now())
+    }
+})
+
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'views', 'index.html'));
+    res.render('index')
 });
 
 app.get('/cluster-reports', (req, res) => {
-    res.sendFile(path.join(__dirname, 'views', 'cluster-reports.html'));
+    res.render('cluster-reports')
 });
 
 app.get('/state-coord-reports', (req, res) => {
-    res.sendFile(path.join(__dirname, 'views', 'state-coord-reports.html'));
+    res.render('state-coord-reports')
 });
 
-app.post('/cluster-reports', (req, res) => {
-
+app.post('/cluster-reports', async (req, res) => {
+    const clusterreport = new ClusterReport({
+        designation: req.body.designation,
+        officerName: req.body.officerName,
+        officerPhone: req.body.officerPhone,
+        officerEmail: req.body.officerEmail,
+        officerLocation: req.body.officerLocation,
+        participantsPhones: req.body.participantsPhones,
+        participantsEmails: req.body.participantsEmails,
+        participantsWhatsApp: req.body.participantsWhatsApp,
+        morningSessionStat: req.body.morningSessionStat,
+        afternoonSessionStat: req.body.afternoonSessionStat,
+        clusterLocationPoint: req.body.clusterLocationPoint,
+        participantsTestimonies: req.body.participantsTestimonies,
+        totalNumberOfParticipants: req.body.totalNumberOfParticipants,
+        numberOfCorperParticipants: req.body.numberOfCorperParticipants,
+        numberOfUndergraduateParticipants: req.body.numberOfUndergraduateParticipants,
+        numberOfPostgraduateParticipants: req.body.numberOfPostgraduateParticipants,
+        numberOfStaffParticipants: req.body.numberOfStaffParticipants,
+        challenges: req.body.challenges,
+        suggestions: req.body.suggestions,
+        decisionsForChrist: req.body.decisionsForChrist,
+        dateOfReport: Date.now()
+    });
 })
 
 app.post('/state-coord-reports', (req, res) => {
